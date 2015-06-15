29488ef3e52d329fc9ede16105e5c6196b1b3fd2
Sammi De Guzman <sammi@sammdot.ca>
2015-06-15 12:21:01 -0400

    add webpage header and basic styles

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Travel Mapping</title>
+<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+<div id="header">
+<div class="container">
+<input type="checkbox" id="hamburger">
+<label for="hamburger"></label>
+<img src="logo.svg">
+<div id="nav">
+<ul>
+<li><a href="/">Home</a></li>
+<li><a href="/">Traveler Stats</a></li>
+<li><a href="/">Highway Browser</a></li>
+<!--<li><a href="/">Login</a></li>-->
+</ul>
+</div>
+</div>
+</div>
+<div id="main">
+<div class="container">
+<!-- Add content here -->
+</div>
+</div>
+</body>
+</html>
