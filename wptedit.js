e4b24d48269a2110096d3235f6a2af90c89f1124
Sammi De Guzman <sammi@sammdot.ca>
2015-06-25 14:22:27 -0400

    start prototype waypoint editor

---
+++ wptedit.js
@@ -0,0 +1,57 @@
+/* This function will make it easier for us to move waypoints
+ * around, both in the data and in the DOM. */
+Array.prototype.move = function (from, to) {
+	this.splice(to, 0, this.splice(from, 1)[0]);
+};
+NodeList.prototype.splice = Array.prototype.splice;
+NodeList.prototype.move = NodeList.prototype.move;
+
+var visibleWptMarker = {
+	url: "wpt-visible.png",
+	size: new google.maps.Size(8, 8),
+	origin: new google.maps.Point(0, 0),
+	anchor: new google.maps.Point(4, 4)
+};
+var hiddenWptMarker = {
+	url: "wpt-hidden.png",
+	size: new google.maps.Size(8, 8),
+	origin: new google.maps.Point(0, 0),
+	anchor: new google.maps.Point(4, 4)
+};
+
+function loadWaypoints(waypoints, map) {
+	var bounds = null;
+	var path = null;
+	var points = [];
+	var markers = {};
+
+	waypoints.forEach(function (wpt) {
+		var coords = new google.maps.LatLng(wpt.lat, wpt.lng);
+		var marker = new google.maps.Marker({
+			map: map,
+			draggable: true,
+			position: coords,
+			title: wpt.label,
+			icon: (wpt.hidden ? hiddenWptMarker : visibleWptMarker)
+		});
+		if (bounds === null) {
+			bounds = new google.maps.LatLngBounds(coords, coords);
+		} else {
+			bounds.extend(coords);
+		}
+		points.push(coords);
+
+		markers[wpt.label] = marker;
+	});
+
+	var path = new google.maps.Polyline({
+		map: map,
+		path: points,
+		strokeColor: "#214478",
+		strokeOpacity: 0.7
+	});
+
+	map.fitBounds(bounds);
+
+	return markers;
+}
