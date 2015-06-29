a90a59c58e4b19708926e428f34745127f84892f
Sammi De Guzman <sammi@sammdot.ca>
2015-06-28 22:54:43 -0400

    add links to copyright notices

--- maptypes.js
+++ maptypes.js
@@ -35,7 +35,7 @@ var mapTypes = [
 ];
 
 var copyrightNotices = {
-	"OSM Mapnik": "Map data © OpenStreetMap contributors",
-	"MapQuest Open": "Data, imagery and map information provided by MapQuest, OpenStreetMap and contributors, ODbL",
-	"MapQuest Open Sat": "Data, imagery and map information provided by MapQuest, OpenStreetMap and contributors, ODbL"
+	"OSM Mapnik": "© <a target=\"_blank\" href=\"http://www.openstreetmap.org/copyright\">OpenStreetMap</a> and contributors",
+	"MapQuest Open": "Data, imagery and map information provided by MapQuest, <a target=\"_blank\" href=\"http://www.openstreetmap.org/copyright\">OpenStreetMap</a> and contributors, <a target=\"_blank\" href=\"http://wiki.openstreetmap.org/wiki/Legal_FAQ#3a._I_would_like_to_use_OpenStreetMap_maps._How_should_I_credit_you.3F\">ODbL</a>",
+	"MapQuest Open Sat": "Data, imagery and map information provided by MapQuest, <a target=\"_blank\" href=\"http://www.openstreetmap.org/copyright\">OpenStreetMap</a> and contributors, <a target=\"_blank\" href=\"http://wiki.openstreetmap.org/wiki/Legal_FAQ#3a._I_would_like_to_use_OpenStreetMap_maps._How_should_I_credit_you.3F\">ODbL</a>"
 };
