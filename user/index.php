5ec9630a75b01a8d00bc14b4a15d829c5836164e
Thing342 <wesjordansemailaddress@gmail.com>
2015-12-19 12:54:29 -0500

    added 'stats by system' table

--- user/index.php
+++ user/index.php
@@ -1,4 +1,11 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<!-- 
+	A basic user stats page. 
+	URL Params:
+		u - the user
+		order - the way to order fields in the regions table
+		rg - region to view detailed stats for
+-->
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
@@ -21,6 +28,7 @@ top: 80px;
 bottom: 0px;
 width: 100%;
 overflow:auto;
+padding: 20px;
 }
 
 table.nmptable {
@@ -88,6 +96,10 @@ text-align:left;
 			echo "Traveler Stats for ".$user;
 		}
 
+		if (array_key_exists("rg", $_GET)) {
+			$rg = $_GET['rg'];
+		}
+
 		$dbname = "TravelMapping";
 		if (array_key_exists("db",$_GET)) {
 		  $dbname = $_GET['db'];
@@ -105,6 +117,10 @@ text-align:left;
 		    // search forward starting from end minus needle length characters
 		    return $needle === "" || (($temp = strlen($haystack) - strlen($needle)) >= 0 && strpos($haystack, $needle, $temp) !== FALSE);
 		}
+
+		function colorScale($percent) {
+			
+		}
 	?>
 </title>
 </head>
@@ -115,30 +131,38 @@ text-align:left;
 		<input type="text" name="u" form="userselect" value="<?php echo $user ?>">
 		<input type="submit">
 	</form>
-	<h1>Traveler Stats for <?php echo $user ?>:</h1>
+	<h1>Traveler Stats for 
+	<?php 
+		echo $user;
+		if (!is_null($rg)) {
+			echo " in ".$rg;
+		} 
+	?>
+	:</h1>
 	</div>
 	<div id="body">
+		<h2>Stats by Region
 		<table class="gratable">
 			<thead>
 				<tr>
 					<th colspan="5">Clinched Mileage by Region:</th>
 				</tr>
 				<tr>
-					<th><a href="?<?php echo "u=".$user."&order=region" ?>">Region</a></th>
-					<th><a href="?<?php echo "u=".$user."&order=clinchedMileage desc" ?>">Clinched Mileage</a></th>
-					<th><a href="?<?php echo "u=".$user."&order=totalMileage desc" ?>">Overall Mileage</a></th>
-					<th><a href="?<?php echo "u=".$user."&order=percentage" ?>">Percent Clinched</a></th>
+					<th><a href="?<?php echo "u=".$user."&rorder=region" ?>">Region</a></th>
+					<th><a href="?<?php echo "u=".$user."&rorder=clinchedMileage desc" ?>">Clinched Mileage</a></th>
+					<th><a href="?<?php echo "u=".$user."&rorder=totalMileage desc" ?>">Overall Mileage</a></th>
+					<th><a href="?<?php echo "u=".$user."&rorder=percentage" ?>">Percent Clinched</a></th>
 					<th>Map</th>
 				</tr>
 			</thead>
 			<tbody>
 				<?php
 					$sql_command = "SELECT o.region, co.mileage as clinchedMileage, o.mileage as totalMileage FROM overallMileageByRegion AS o INNER JOIN clinchedOverallMileageByRegion AS co ON co.region = o.region WHERE co.traveler = '".$user."' ORDER BY ";
-					if (array_key_exists("order", $_GET) && strlen($_GET["order"]) > 0) {
-						if (!strcmp($_GET['order'], "percentage")) {
+					if (array_key_exists("rorder", $_GET) && strlen($_GET["rorder"]) > 0) {
+						if (!strcmp($_GET['rorder'], "percentage")) {
 							$sql_command .= "1 - clinchedMileage / totalMileage";
 						} else {
-							$sql_command .= $_GET["order"];
+							$sql_command .= $_GET["rorder"];
 						}
 				    } else {
 				    	$sql_command .= "o.region";
@@ -150,6 +174,35 @@ text-align:left;
 				        echo "<tr><td>".$row['region']."</td><td>".$row['clinchedMileage']."</td><td>".$row['totalMileage']."</td><td>".$percent."%</td><td><a href=\"/hbtest/mapview.php?u=".$user."&rg=".$row['region']."\">Map</a></td></tr>";
 				    }
 			        $res->free();
+				?>
+				<tr><td colspan="5">*Regions with no mileage not shown</td>
+			</tbody>
+		</table>
+		<h2>Stats by System</h2>
+		<table class="gratable">
+			<thead>
+				<tr>
+					<th colspan="7">Clinched Mileage by System</th>
+				</tr>
+				<tr>
+					<th>Country</th>
+					<th>System Code</th>
+					<th>System Name</th>
+					<th>Clinched Mileage</th>
+					<th>Overall Mileage</th>
+					<th>Percent Clinched</th>
+					<th>Map</th>
+				</tr>
+			</thead>
+			<tbody>
+				<?php 
+					$sql_command = "SELECT sys.countryCode, sys.systemName, sys.fullName, r.root, COALESCE(ROUND(SUM(cr.mileage), 2),0) AS clinchedMileage, COALESCE(ROUND(SUM(r.mileage), 2), 0) AS totalMileage, COALESCE(ROUND(SUM(cr.mileage) / SUM(r.mileage) * 100, 3), 0) AS percentage FROM systems as sys INNER JOIN routes AS r ON r.systemName = sys.systemName LEFT JOIN clinchedRoutes AS cr ON cr.route = r.root AND cr.traveler = '".$user."' GROUP BY r.systemName ORDER BY sys.countryCode DESC, sys.tier, sys.systemName;";
+					echo "<!-- SQL: ".$sql_command."-->";
+					$res = $db->query($sql_command);
+					while ($row = $res->fetch_assoc()) {
+						echo "<tr><td>".$row['countryCode']."</td><td>".$row['systemName']."</td><td>".$row['fullName']."</td><td>".$row['clinchedMileage']."</td><td>".$row['totalMileage']."</td><td>".$row['percentage']."</td><td><a href=\"/hbtest/mapview.php?u=".$user."&sys=".$row['systemName']."\">Map</a></td></tr>";
+					}
+					$res->free();
 				?>
 			</tbody>
 		</table>
