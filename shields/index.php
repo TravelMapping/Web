537accae83cfc84885752cb9b52b6673166c1d81
Thing342 <wesjordansemailaddress@gmail.com>
2015-12-24 14:35:03 -0500

    added usai, usaif, and usaus support

---
+++ shields/index.php
@@ -0,0 +1,35 @@
+<?php
+	header("Content-type: image/svg+xml");
+
+	$r = $_GET['r'];
+	$db = new mysqli("localhost","travmap","clinch","TravelMapping") or die("Failed to connect to database");
+	$sql_command = "SELECT * FROM routes WHERE root = '".$r."';";
+	$res = $db->query($sql_command);
+	$row = $res->fetch_assoc();
+
+	$svg = file_get_contents("template_".$row['systemName'].".svg");
+
+	switch ($row['systemName']) {
+		case 'usai': case 'usaif':
+			$routeNum = explode("-", $row['route'])[1];
+			if (strlen($routeNum) > 2) {
+				$svg = file_get_contents("template_usai_wide.svg");
+			}
+			$svg = str_replace("***NUMBER***", $routeNum, $svg);
+			echo $svg;
+			break;
+
+		case 'usaus':
+			$routeNum = str_replace("US", "", $row['route']);
+			if (strlen($routeNum) > 2) {
+				$svg = file_get_contents("template_usaus_wide.svg");
+			}
+			$svg = str_replace("***NUMBER***", $routeNum, $svg);
+			echo $svg;
+			break;
+		
+		default:
+			# code...
+			break;
+	}	
+?>
