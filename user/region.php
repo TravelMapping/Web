7b7f9f23aee1915d0446544ad878c76f2dc68dff
Jim Teresco <terescoj@gmail.com>
2016-06-03 00:55:07 -0400

    Updates to use consolidated DB.

--- user/region.php
+++ user/region.php
@@ -98,19 +98,6 @@
     echo "<!--".$sql_command."-->";
     $regionInfo = mysql_fetch_array(mysql_query($sql_command));
 
-    # functions from http://stackoverflow.com/questions/834303/startswith-and-endswith-functions-in-php
-    function startsWith($haystack, $needle)
-    {
-        // search backwards starting from haystack length characters from the end
-        return $needle === "" || strrpos($haystack, $needle, -strlen($haystack)) !== FALSE;
-    }
-
-    function endsWith($haystack, $needle)
-    {
-        // search forward starting from end minus needle length characters
-        return $needle === "" || (($temp = strlen($haystack) - strlen($needle)) >= 0 && strpos($haystack, $needle, $temp) !== FALSE);
-    }
-
     function fetchWithRank($res, $rankBy)
     {
         global $user;
