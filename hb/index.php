3ca111ce12d87eaba626226061807ace841c377a
Thing342 <wesjordansemailaddress@gmail.com>
2016-07-10 10:16:50 -0400

    Added route statistics to hb

--- hb/index.php
+++ hb/index.php
@@ -85,6 +85,15 @@
             margin: 5px;
         }
 
+        #pointbox table {
+            width: 75%;
+            margin-bottom: 15px;
+        }
+
+        #routeInfo td {
+            text-align: right;
+        }
+
 
     </style>
     <?php require $_SERVER['DOCUMENT_ROOT']."/lib/tmphpfuncs.php" ?>
@@ -279,14 +288,64 @@ if ($routeparam != "") {
     echo "<div id=\"pointbox\">\n";
     echo "<span class='bigshield'>" . generate($_GET['r'], true) . "</span>";
     echo "<span><a href='/user/mapview.php?u={$_GET['u']}&amp;rte={$routeInfo['route']}'>View Associated Routes</a></span>";
-    echo "<table id='waypoints' class=\"gratable\"><thead><tr><th colspan=\"2\">Waypoints</th></tr><tr><th>Coordinates</th><th>Waypoint Name</th></tr></thead><tbody>\n";
-    $sql_command = "SELECT pointName, latitude, longitude FROM waypoints WHERE root = '".$routeparam."';";
+    echo "<table id='routeInfo' class=\"gratable\"><thead><tr><th colspan='2'>Route Stats</th></tr></thead><tbody>";
+    $sql_command = <<<SQL
+      SELECT
+          COUNT(*) as numDrivers,
+          SUM(cr.clinched) as numClinched,
+          @numUsers := (
+            SELECT COUNT(DISTINCT traveler) FROM clinchedOverallMileageByRegion
+          ) as numUsers,
+          ROUND(COUNT(*) / @numUsers * 100, 2) as drivenPct,
+          ROUND(SUM(cr.clinched) / @numUsers * 100, 2) as clinchedPct,
+          ROUND(SUM(cr.clinched) / COUNT(*) * 100, 2) as drivenClinchedPct,
+          GROUP_CONCAT(cr.traveler SEPARATOR ', ') as drivers,
+          GROUP_CONCAT(IF(cr.clinched = 1, cr.traveler, null) separator ', ') as clinchers,
+          ROUND(AVG(cr.mileage), 2) as avgMileage,
+          ROUND(r.mileage, 2) as totalMileage,
+          ROUND(avg(cr.mileage) / r.mileage * 100, 2) as mileagePct
+        FROM clinchedRoutes as cr
+          JOIN routes as r ON cr.route = r.root
+        WHERE cr.route = '$routeparam'
+SQL;
+    $row = tmdb_query($sql_command) -> fetch_assoc();
+    echo <<<HTML
+    <tr><td class="important">Total Mileage</td><td>{$row['totalMileage']} mi</td></tr>
+    <tr><td>LIST Name</td><td>{$routeInfo['region']} {$routeInfo['route']}{$routeInfo['banner']}{$routeInfo['abbrev']}</td></tr> 
+    <tr><td>Average Mileage</td><td>{$row['avgMileage']} mi ({$row['mileagePct']} %)</td>
+    <tr title="{$row['drivers']}"><td>Total Drivers</td><td>{$row['numDrivers']} ({$row['drivenPct']} %)</td>
+    <tr class="link" title="{$row['clinchers']}"><td rowspan="2">Total Clinched</td><td>{$row['numClinched']} ({$row['clinchedPct']} %)</td>
+    <tr class="link" title="{$row['clinchers']}"><td>{$row['drivenClinchedPct']} % of drivers</td>
+    </tbody></table>
+HTML;
+    echo "<table id='waypoints' class=\"gratable\"><thead><tr><th colspan=\"3\">Waypoints</th></tr><tr><th>Coordinates</th><th>Name</th><th title='Percent of people who have driven this route who have driven though this point.'>%</th></tr></thead><tbody>\n";
+    $sql_command = <<<SQL
+        SELECT pointName, latitude, longitude, driverPercent
+        FROM waypoints
+        LEFT JOIN (
+            SELECT
+              waypoints.pointId,
+              @num_drivers := (
+                SELECT COUNT(DISTINCT traveler) FROM clinchedRoutes WHERE clinchedRoutes.route = '$routeparam'
+              ) as numDrivers,
+              count(*),
+              ROUND(count(*) / @num_drivers * 100, 2) as driverPercent
+            FROM segments
+            LEFT JOIN clinched ON segments.segmentId = clinched.segmentId
+            LEFT JOIN waypoints ON segments.waypoint1 = waypoints.pointId
+            WHERE segments.root = '$routeparam'
+            GROUP BY segments.segmentId
+        ) as pointStats on pointStats.pointId = waypoints.pointId
+        WHERE root = '$routeparam';
+SQL;
     $res = tmdb_query($sql_command);
     $waypointnum = 0;
     while ($row = $res->fetch_assoc()) {
         # only visible points should be in this table
         if (!startsWith($row['pointName'], "+")) {
-            echo "<tr><td>(" . $row['latitude'] . "," . $row['longitude'] . ")</td><td class='link'><a onClick='javascript:LabelClick(" . $waypointnum . ",\"" . $row['pointName'] . "\"," . $row['latitude'] . "," . $row['longitude'] . ",0);'>" . $row['pointName'] . "</a></td></tr>\n";
+            $colorFactor = $row['driverPercent'] / 100;
+            $colors = [255, 255 - round($colorFactor * 128), 255 - round($colorFactor * 128)];
+            echo "<tr><td>(" . $row['latitude'] . "," . $row['longitude'] . ")</td><td class='link'><a onClick='javascript:LabelClick(" . $waypointnum . ",\"" . $row['pointName'] . "\"," . $row['latitude'] . "," . $row['longitude'] . ",0);'>" . $row['pointName'] . "</a></td><td style='background-color: rgb({$colors[0]},{$colors[1]},{$colors[2]})'>{$row['driverPercent']}</td></tr>\n";
         }
         $waypointnum = $waypointnum + 1;
     }
