6bfb88c579dada6a7dc96764d6fb0b0bc207d099
theFXexpert <danielb459@verizon.net>
2017-01-21 12:02:41 -0500

    datacheck.php favicon

--- devel/datacheck.php
+++ devel/datacheck.php
@@ -4,6 +4,7 @@
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 <link rel="stylesheet" type="text/css" href="/css/travelMapping.css" />
+<link rel="shortcut icon" type="image/png" href="/favicon.png">
 <style type="text/css">
 </style>
 <?php
