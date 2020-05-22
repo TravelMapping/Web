1b61f48649ac1917c90edb4e500cdef9dbf5fdff
Jim Teresco <terescoj@gmail.com>
2020-05-21 22:25:26 -0400

    Remove some now obsolete code. #212

--- lib/getVisibleSegments.php
+++ lib/getVisibleSegments.php
@@ -1,7 +1,8 @@
 <?
 // read information from the TM database about all segments
 // that have at least one waypoint endpoint within the
-// latitude/longitude bounds given.
+// latitude/longitude bounds given, and then information about
+// each route that those segments are parts of
 //
 $params = json_decode($_POST['params'], true);
 
@@ -16,7 +17,6 @@ ob_end_clean();
 
 // initialize the array of responses
 $response = array('roots'=>array(),
-                  'routes'=>array(),
 		  'w1name'=>array(),
 		  'w1lat'=>array(),
 		  'w1lng'=>array(),
@@ -29,7 +29,7 @@ $response = array('roots'=>array(),
 
 // make DB query for all segments with at least one waypoint in
 // the bounding area
-$result = tmdb_query("select segments.root, r.region, r.route, r.banner, r.abbrev, r.city, w1.pointName as w1name, w1.latitude as w1lat, w1.longitude as w1lng, w2.pointName as w2name, w2.latitude as w2lat, w2.longitude as w2lng from segments join waypoints as w1 on segments.waypoint1=w1.pointId join waypoints as w2 on segments.waypoint2=w2.pointId join routes as r where ((w1.latitude>".$params['minLat']." and w1.latitude<".$params['maxLat']." and w1.longitude<".$params['maxLng']." and w1.longitude>".$params['minLng'].") or (w2.latitude>".$params['minLat']." and w2.latitude<".$params['maxLat']." and w2.longitude<".$params['maxLng']." and w2.longitude>".$params['minLng'].")) and r.root=segments.root order by r.csvOrder;");
+$result = tmdb_query("select segments.root, w1.pointName as w1name, w1.latitude as w1lat, w1.longitude as w1lng, w2.pointName as w2name, w2.latitude as w2lat, w2.longitude as w2lng from segments join waypoints as w1 on segments.waypoint1=w1.pointId join waypoints as w2 on segments.waypoint2=w2.pointId where ((w1.latitude>".$params['minLat']." and w1.latitude<".$params['maxLat']." and w1.longitude<".$params['maxLng']." and w1.longitude>".$params['minLng'].") or (w2.latitude>".$params['minLat']." and w2.latitude<".$params['maxLat']." and w2.longitude<".$params['maxLng']." and w2.longitude>".$params['minLng']."));");
 
 // temp array of unique root values of the segments
 $allroots = array();
@@ -42,7 +42,6 @@ while ($row = $result->fetch_assoc()) {
     if (!in_array($row['root'], $allroots)) {
        array_push($allroots, $row['root']);
     }
-    array_push($response['routes'], $row['region']." ".$row['route'].$row['banner'].$row['abbrev']);
     array_push($response['w1name'], $row['w1name']);
     array_push($response['w1lat'], $row['w1lat']);
     array_push($response['w1lng'], $row['w1lng']);
