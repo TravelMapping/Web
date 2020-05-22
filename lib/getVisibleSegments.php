7054abf4c273c2d777d617b8345732f54e9deea5
Jim Teresco <terescoj@gmail.com>
2020-05-21 22:22:18 -0400

    Separate query for route info. #212

--- lib/getVisibleSegments.php
+++ lib/getVisibleSegments.php
@@ -17,22 +17,31 @@ ob_end_clean();
 // initialize the array of responses
 $response = array('roots'=>array(),
                   'routes'=>array(),
-		  'cities'=>array(),
 		  'w1name'=>array(),
 		  'w1lat'=>array(),
 		  'w1lng'=>array(),
 		  'w2name'=>array(),
 		  'w2lat'=>array(),
-		  'w2lng'=>array()
+		  'w2lng'=>array(),
+		  'routeroots'=>array(),
+		  'routelistnames'=>array()
 		  );
 
-// make DB query
+// make DB query for all segments with at least one waypoint in
+// the bounding area
 $result = tmdb_query("select segments.root, r.region, r.route, r.banner, r.abbrev, r.city, w1.pointName as w1name, w1.latitude as w1lat, w1.longitude as w1lng, w2.pointName as w2name, w2.latitude as w2lat, w2.longitude as w2lng from segments join waypoints as w1 on segments.waypoint1=w1.pointId join waypoints as w2 on segments.waypoint2=w2.pointId join routes as r where ((w1.latitude>".$params['minLat']." and w1.latitude<".$params['maxLat']." and w1.longitude<".$params['maxLng']." and w1.longitude>".$params['minLng'].") or (w2.latitude>".$params['minLat']." and w2.latitude<".$params['maxLat']." and w2.longitude<".$params['maxLng']." and w2.longitude>".$params['minLng'].")) and r.root=segments.root order by r.csvOrder;");
+
+// temp array of unique root values of the segments
+$allroots = array();
 
 // parse results into the response array
 while ($row = $result->fetch_assoc()) {
 
     array_push($response['roots'], $row['root']);
+    // did we find a new root we haven't seen before?
+    if (!in_array($row['root'], $allroots)) {
+       array_push($allroots, $row['root']);
+    }
     array_push($response['routes'], $row['region']." ".$row['route'].$row['banner'].$row['abbrev']);
     array_push($response['w1name'], $row['w1name']);
     array_push($response['w1lat'], $row['w1lat']);
@@ -43,6 +52,16 @@ while ($row = $result->fetch_assoc()) {
 }
 
 $result->free();
+
+// build a query to get information about all of the routes
+$result = tmdb_query("select * from routes where root in ('".implode("','",$allroots)."');");
+
+// parse results into the response array
+while ($row = $result->fetch_assoc()) {
+      array_push($response['routeroots'], $row['root']);
+      array_push($response['routelistnames'], $row['region']." ".$row['route'].$row['banner'].$row['abbrev']);
+}
+
 $tmdb->close();
 echo json_encode($response);
 ?>
