b631078d389b8d364b8c86be375d199c764aacdd
jteresco <terescoj@gmail.com>
2022-06-02 14:05:04 -0400

    Wording on findroute header regarding sortable capabilities.

--- hb/findroute.php
+++ hb/findroute.php
@@ -89,14 +89,15 @@ require  $_SERVER['DOCUMENT_ROOT']."/lib/tmheader.php";
 <h1>Travel Mapping Route Finder</h1>
 <p class="text" id="frIntro">
 Use the table below to find routes from among the
-<?php $r = number_format(tm_count_rows("routes", "")); echo $r; ?> routes in TM's database.
-You can filter by most categories.  To filter by route name, type a
-substring of its name in the box below "Route Name" then hit Enter.
-Initial loading may take several seconds to a few minutes depending on
-the server and network conditions and your browser's capabilities.
-Table updates after changing the filters can also take a few seconds.
-Links under ".list Name" will take you to the map and other details of that
-route.
+<?php $r = number_format(tm_count_rows("routes", "")); echo $r; ?>
+routes in TM's database.  You can filter by most categories.  To
+filter by route name, type a substring of its name in the box below
+"Route Name" then hit Enter.  Initial loading may take several seconds
+to a few minutes depending on the server and network conditions and
+your browser's capabilities.  Table updates after changing the filters
+can also take a few seconds.  If enabled, column sorting can be memory
+intensive and sorting can take a few seconds.  Links under ".list
+Name" will take you to the map and other details of that route.
 <?php tm_dismiss_button("frIntro"); ?>
 </p>
 <div id="choppedbox">
