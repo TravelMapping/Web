20b28d786f71452ffd0028f7c882737ce8ed6d10
jteresco <terescoj@gmail.com>
2023-01-30 23:40:22 -0500

    Initial implementation for #724

--- graphs/index.php
+++ graphs/index.php
@@ -4,7 +4,30 @@
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <?php require $_SERVER['DOCUMENT_ROOT']."/lib/tmphpfuncs.php" ?>
-<title>Travel Mapping Graph Data</title>
+<?php
+$archiveSet = "";
+if (array_key_exists("gv", $_GET)) {
+    $archiveSet = strtolower($_GET['gv']);
+}
+
+// build info about graph archive sets
+$result = tmdb_query("SELECT * from graphArchiveSets");
+$currIndex = 0;
+$matchIndex = -1;
+$graphPath = "../graphdata/";
+$archiveSets = array('setName'=>array(), 'descr'=>array());
+while ($row = $result->fetch_array()) {
+    array_push($archiveSets['setName'], $row['setName']);
+    array_push($archiveSets['descr'], $row['descr']);
+    if ($row['setName'] == $archiveSet) {
+        $matchIndex = $currIndex;
+	$graphPath = "../grapharchives/".$archiveSet."/";
+    }
+    $currIndex = $currIndex + 1;
+}
+$result->free();
+?>
+<title>Travel Mapping/METAL Graph Data</title>
 <link rel="stylesheet" type="text/css" href="/css/travelMapping.css">
 <link rel="stylesheet" type="text/css" href="//cdn.datatables.net/1.10.15/css/jquery.dataTables.min.css">
 <style>
@@ -18,29 +41,32 @@ padding:0px;
 <body>
 <?php require  $_SERVER['DOCUMENT_ROOT']."/lib/tmheader.php"; ?>
 
-<p class="heading">Travel Mapping Graph Data</p>
+<p class="heading">Travel Mapping/METAL Graph Data</p>
 
 <div class="text"> 
 
-This is the repository of graphs generated from the current <a
-href="/">Travel Mapping Project</a> data.  These were created for use
-in the <a href="https://courses.teresco.org/metal/">Map-based
-Educational Tools for Algorithm Learning (METAL)</a> academic project.
-You may contact <a href="https://j.teresco.org/">the author</a> if you
-would like an archive of some or all of the graphs, or if you have
-special requests for a graph of specific subset of the project's data
-to be generated.  If you do decide to make use of these graphs and/or
-the tools described here, please drop a note to <a
-href="https://j.teresco.org/">the author</a>.
+  These graphs represent subsets of highway networks around the world.
+  They are generated daily for
+  the <a href="https://courses.teresco.org/metal/">Map-based
+  Educational Tools for Algorithm Learning (METAL)</a> academic
+  project using highway data from the <a href="/">Travel Mapping
+    Project</a>.  Details about graph data and the file formats are
+  <a href="https://courses.teresco.org/metal/graph-formats.shtml">here</a>.  
+  
+</div>
+  
+<div class="text">
+
+Contact <a href="https://j.teresco.org/">the METAL project leader</a> with
+questions or concerns, and please drop a note if you find these graphs and the related tools to be useful.
 
 </div>
 
 <div class="text">
 
-For each graph, the table below gives a brief
-description, then download links and graph information for the default (collapsed), traveled, and
-simple <a
-href="https://courses.teresco.org/metal/graph-formats.shtml">formats</a>.
+For each graph, the table below gives a brief description, then
+download links and graph information for the default (collapsed),
+traveled, and simple formats.
 <b>Note:</b> larger graphs
 greatly tax the Highway Data Examiner and the Leaflet maps.  They
 should all work, but large graphs require a lot of memory for your
@@ -49,9 +75,8 @@ browser and some patience.
 
 <div class="text">
 
-If you would like a local copy of all of the latest graphs, you can
-download <a href="../graphdata/graphs.zip">a zip archive of all of them</a>.
-Beware that this is a large file!
+You can download <a href="../graphdata/graphs.zip">a zip archive of
+all of the latest graphs</a>.  Beware that this is a large file!
 
 </div>
 
@@ -61,81 +86,112 @@ Graphs are copyright &copy; <a href="https://j.teresco.org/">James
 D. Teresco</a>, generated from highway data gathered and maintained by
 <a href="https://travelmapping.net/credits.php#contributors">Travel
 Mapping Project</a> contributors.  Graphs may be downloaded freely for
-academic use.  Other use by explicit permission only.
+academic use.  Other use is by written permission only.
 
 </div>
-<p class="subheading">All Graphs</p>
+<p class="subheading">
+  <?php
+  if (($archiveSet != "") && ($matchIndex == -1)) {
+      echo "Most Recent Graphs (Archive set ".$archiveSet." not found)";
+      $archiveSet = "";
+  }
+  else if ($archiveSet == "") {
+      echo "Most Recent Graphs";
+  }
+  else {
+      echo $archiveSets['descr'][$matchIndex]." Graphs";
+  }
+  ?>
+</p>
 <p />
 <div style="text-align:center;">
-			<p>Filter by number of vertices (in default/collapsed)</p>
-			from
-			<input type="number" min="1" value="1" id="regMin" style="width:6rem;" onchange="tableFilter(event)">
-			to
-			<input type="number" min="1" value="30000" id="regMax" style="width:6rem;" onchange="tableFilter(event)">
-			vertices
-			<p>Filter by graph type</p>
-			<select id="regFil" onchange="selFilter(event)">
-				<option value="all">All</option>
-			</select>
-			</div>
-			<table class="gratable" id="regTable" border="1">
-			<thead>
-			<tr><th rowspan="2">Graph Description</th><th colspan="3">Default (Collapsed) Format Graph</th><th colspan="4">Traveled Format Graph</th><th colspan="3">Simple Format Graph</th></tr>
-			<tr><th>Download Link</th><th>Vertices</th><th>Edges</th><th>Download Link</th><th>Vertices</th><th>Edges</th><th>Travelers</th><th>Download Link</th><th>Vertices</th><th>Edges</th></tr></thead>
-			<tbody id="regBody">
-<?php
-$tmconffile = fopen($_SERVER['DOCUMENT_ROOT']."/lib/tm.conf", "r");
-$tmdbname = chop(fgets($tmconffile));
-$tmdbuser = chop(fgets($tmconffile));
-$tmdbpasswd = chop(fgets($tmconffile));
-$tmdbhost = chop(fgets($tmconffile));
+  <p>Filter by number of vertices (in default/collapsed)</p>
+  from
+  <input type="number" min="1" value="1" id="regMin" style="width:6rem;" onchange="tableFilter(event)">
+    to
+    <input type="number" min="1" value="30000" id="regMax" style="width:6rem;" onchange="tableFilter(event)">
+      vertices
+      <p>Filter by graph type</p>
+      <select id="regFil" onchange="selFilter(event)">
+	<option value="all">All</option>
+      </select>
+    </div>
+    <table class="gratable" id="regTable" border="1">
+      <thead>
+	<tr>
+	  <th rowspan="2">Graph Description</th>
+	  <th colspan="3">Default (Collapsed) Format Graph</th>
+	  <?php
+	  if ($archiveSet != "") {
+	     echo "<th colspan=\"3\">Intersection (Collapsed) Format Graph</th>";
+	  }
+	  ?>
+	  <th colspan="4">Traveled Format Graph</th>
+	  <th colspan="3">Simple Format Graph</th></tr>
+	<tr>
+	  <?php
+	  if ($archiveSet != "") {
+	     echo "<th>Download Link</th><th>Vertices</th><th>Edges</th>";
+	  }
+	  ?>
+	  <th>Download Link</th><th>Vertices</th><th>Edges</th>
+	  <th>Download Link</th><th>Vertices</th><th>Edges</th><th>Travelers</th>
+	  <th>Download Link</th><th>Vertices</th><th>Edges</th>
+	</tr>
+      </thead>
+      <tbody id="regBody">
+	<?php
+	if ($archiveSet == "") {
+	   $result = $tmdb->query("SELECT * FROM graphs ORDER BY descr, format");
+        }
+	else {
+	   $result = $tmdb->query("SELECT * FROM graphArchives WHERE setName='".$archiveSet."' ORDER BY descr, format");
+        }
+	$counter = 0;
+	$prevRow;  // what is this?
 
+	while ($counter < $result->num_rows) {
 
-// make the connection
-//echo "<!-- mysqli connecting to database ".$tmdbname." on ".$tmdbhost." -->\n";
-mysqli_report(MYSQLI_REPORT_STRICT);
-try {
-    $tmdb = new mysqli($tmdbhost, $tmdbuser, $tmdbpasswd, $tmdbname);
-}
-catch ( Exception $e ) {
-   //echoecho "<h1 style='color: red'>Failed to connect to database ".$tmdbname." on ".$tmdbhost." Please try again later.</h1>";
-   exit;
-}
-$result = $tmdb->query("SELECT * FROM graphs ORDER BY descr, format");
-$counter = 0;
-$prevRow;
+	    // get three or four entries: collapsed, (intonly), simple,
+      	    // then traveled for each
+      	    $crow = $result->fetch_assoc();
+	    if ($archiveSet != "") {
+	       $irow = $result->fetch_assoc();
+	       $counter += 1;
+            }
+      	    $srow = $result->fetch_assoc();
+      	    $trow = $result->fetch_assoc();
 
-while ($counter < $result->num_rows) {
+      	    $counter += 3;
 
-      // get three entries: collapsed, simple, then traveled for each
-      $crow = $result->fetch_assoc();
-      $srow = $result->fetch_assoc();
-      $trow = $result->fetch_assoc();
+      	    if ($crow == NULL || $srow == NULL || $trow == NULL) {
+               // should produce some kind of error message
+               continue;
+      	    }
 
-      $counter += 3;
-
-      if ($crow == NULL || $srow == NULL || $trow == NULL) {
-          // should produce some kind of error message
-          continue;
-      }
-
-      // build table row (was: class=collapsed)
-      echo "<tr class='".$crow['category']."'>
-	   <td>".$crow['descr']."</td>
-	   <td><a href=\"../graphdata/".$crow['filename']."\">".$crow['filename']."</a></td>
+      	    // build table row (was: class=collapsed)
+      	    echo "<tr class='".$crow['category']."'>
+	    <td>".$crow['descr']."</td>
+	    <td><a href=\"".$graphPath.$crow['filename']."\">".$crow['filename']."</a></td>
 	   <td class='c".$crow['vertices']."'>".$crow['vertices']."</td>
 	   <td>".$crow['edges']."</td>\n";
+
+	   if ($archiveSet != "") {
+	       echo "<td><a href=\"".$graphPath.$irow['filename']."\">".$irow['filename']."</a></td>
+	       <td class='c".$irow['vertices']."'>".$irow['vertices']."</td>
+	       <td>".$irow['edges']."</td>\n";
+	    }
 		
-      echo "<td><a href=\"../graphdata/".$trow['filename']."\">".$trow['filename']."</a></td>
+	   echo "<td><a href=\"".$graphPath.$trow['filename']."\">".$trow['filename']."</a></td>
 	   <td>".$trow['vertices']."</td>
 	   <td>".$trow['edges']."</td>
 	   <td>".$trow['travelers']."</td>\n";
 
-      echo "<td><a href=\"../graphdata/".$srow['filename']."\">".$srow['filename']."</a></td>
+	   echo "<td><a href=\"".$graphPath.$srow['filename']."\">".$srow['filename']."</a></td>
 	   <td>".$srow['vertices']."</td>
 	   <td>".$srow['edges']."</td>
            </tr>\n";
-}
+	  }
 $values = array();
 $descr = array();
 $result = $tmdb->query("SELECT * FROM graphTypes");
