2a455903f3b22db5b881b1cce4f7b44680c68102
jteresco <terescoj@gmail.com>
2025-06-12 22:25:39 -0400

    Showroute units. Fixes #826 #548 #565

--- hb/showroute.php
+++ hb/showroute.php
@@ -1,4 +1,5 @@
 <?php require $_SERVER['DOCUMENT_ROOT']."/lib/tmphpuser.php" ?>
+<?php require $_SERVER['DOCUMENT_ROOT']."/lib/tmphpfuncs.php" ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <!--
  ***
@@ -164,7 +165,6 @@
 	     color: #fff;
 	}
     </style>
-    <?php require $_SERVER['DOCUMENT_ROOT']."/lib/tmphpfuncs.php" ?>
     <?php
     function showroute_error($msg) {
 
