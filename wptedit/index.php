0e22fa739a9eae40ae116c9bd22a218b4ddb9e72
cockroachking <105306113+cockroachking@users.noreply.github.com>
2026-02-03 19:34:02 -0500

    Update index.php

--- wptedit/index.php
+++ wptedit/index.php
@@ -4,6 +4,7 @@
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 
 <style type="text/css">
+<link rel="shortcut icon" type="image/png" href="/favicon.png">
 
 body, html {
   margin:0;
